4af6c27e6fb356a8243bae24e51a6fbf11b401d4
ztskhvaradze <93708582+ztskhvaradze@users.noreply.github.com>
2022-10-08 14:28:00 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ streamlit.title("My parent new healthy Diner")
 
 streamlit.header("Breakfast Menu")
 streamlit.text("Omega 3 & Blubarry oatmeal")
-streamlit.header("smothie")
-streamlit.header("Boiled egg")
+streamlit.text("smothie")
+streamlit.text("Boiled egg")
